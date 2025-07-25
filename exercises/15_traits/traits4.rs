9106bf20ef4fe32cb84014af8aad2042f02ce600
7sevenqi <2661942027@qq.com>
2025-07-25 18:25:24 +0800

    9

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -10,13 +10,13 @@ struct OtherSoftware;
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+// 使用 trait 对象，接受任何实现了 Licensed trait 的引用
+fn compare_license_types(software1: &dyn Licensed, software2: &dyn Licensed) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
 fn main() {
-    // You can optionally experiment here.
+    // 可选：在此处进行实验
 }
 
 #[cfg(test)]
@@ -25,12 +25,11 @@ mod tests {
 
     #[test]
     fn compare_license_information() {
-        assert!(compare_license_types(SomeSoftware, OtherSoftware));
+        assert!(compare_license_types(&SomeSoftware, &OtherSoftware));
     }
 
     #[test]
     fn compare_license_information_backwards() {
-        assert!(compare_license_types(OtherSoftware, SomeSoftware));
+        assert!(compare_license_types(&OtherSoftware, &SomeSoftware));
     }
 }
-
