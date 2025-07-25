16229efb94718ee5f280bcd6f348316fb31ab98b
7sevenqi <2661942027@qq.com>
2025-07-25 18:09:29 +0800

    完成

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,7 +1,7 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
-
+    let a = [0; 100]; 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
@@ -9,4 +9,3 @@ fn main() {
         panic!("Array not big enough, more elements needed");
     }
 }
-
